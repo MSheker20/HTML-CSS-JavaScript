6d5a3060cb43b692e838cb13ac4068266b687391
Mustafa Şeker <mustafa652seker@gmail.com>
2025-09-30 22:23:01 +0300

    feat: add initial HTML and JavaScript files for Variables and Constants lesson

---
+++ JavaScript/02-Variables and Constants/script.js
@@ -0,0 +1,43 @@
+
+
+//Variables
+
+var password;
+console.log(password)
+
+password = "seker123"
+console.log(password)
+
+age = 25;
+console.log(age)
+
+//Variable Naming Rules
+
+//Variables cannot start with numbers
+
+yas1 = 30;
+
+//Variables name cannot be a reserved keyword
+//var var = 10; //SyntaxError: Unexpected token 'var'
+
+//Variables cannot contain spaces
+//var first name = "Mustafa"; //SyntaxError: Unexpected identifier
+
+var first_name;
+first_name = "Mustafa";
+console.log(first_name)
+
+//Variables name can contain special characters except $ and turkish characters
+var $surname = "YILMAZ";
+var yaş = 30;
+console.log($surname, yaş)
+
+//Constants
+
+const email = "mustafa@example.com";
+console.log(email)
+
+email = "mustafa@newdomain.com";
+console.log(email)
+
+
