20bd4014fb801ccf613705b3ba57279eb381e269
Mustafa Şeker <mustafa652seker@gmail.com>
2025-10-01 16:18:57 +0300

    feat: add initial HTML and JavaScript files for Variable Types lesson

---
+++ JavaScript/03-Variable Types/script.js
@@ -0,0 +1,39 @@
+//Variable Types in JavaScript
+
+//Primitive Types
+
+//String
+let firstName = "Mustafa";
+console.log(typeof firstName)
+
+//Number
+let age = 21;
+console.log(typeof age)
+
+//Boolean
+let isCreditUsed = true;
+console.log(typeof isCreditUsed)
+
+//Undefined
+let phoneNumber;
+console.log(typeof phoneNumber)
+
+// Reference Types
+
+//Array
+let hobbies = ["Music", "Movies", "Sports"];
+console.log(typeof hobbies)
+
+//Object Literal
+let address = {
+    city: "Denizli",
+    country: "Türkiye"
+};
+console.log(typeof address)
+
+
+//Function
+var calculate = function() {
+    return 0;
+}
+console.log(typeof calculate)
