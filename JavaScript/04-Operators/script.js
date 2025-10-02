177e9784b6950d6957dac4b7d4569e01a5023ef3
Mustafa Şeker <mustafa652seker@gmail.com>
2025-10-02 11:53:13 +0300

    feat: add initial HTML and JavaScript files for Operators lesson

---
+++ JavaScript/04-Operators/script.js
@@ -0,0 +1,104 @@
+// OPERATORS IN JAVASCRIPT
+
+// 1. Arithmetic Operators
+
+let data1;
+const a = 5;
+const b = 2;
+
+data1 = a + b;
+console.log(data1);
+console.log(typeof data1);
+
+let data2;
+data2 = a - b;
+console.log(data2);
+console.log(typeof data2);
+
+let data3;
+data3 = a * b;
+console.log(data3);
+console.log(typeof data3);
+
+let data4;
+data4 = a / b;
+console.log(data4);
+console.log(typeof data4);
+
+let data5;
+data5 = a % b; // modulus operator
+console.log(data5);
+console.log(typeof data5);
+
+let data6;
+data6 = a ** b; // exponentiation operator
+console.log(data6);
+console.log(typeof data6);
+
+// 2. Assignment Operators
+let data7;
+
+data7 = 10;
+console.log(data7);
+
+data7 += 5;
+console.log(data7);
+
+data7 -= 3;
+console.log(data7);
+
+data7 *= 2;
+console.log(data7);
+
+data7 /= 4;
+console.log(data7);
+
+data7 = 10;
+data7 %= 3;
+console.log(data7);
+
+data7 = 2;
+data7 **= 3;
+console.log(data7);
+
+//3. Comparison Operators
+
+let data8;
+data8 = 5 == '5'; // loose equality operator
+console.log(data8);
+console.log(typeof data8);
+
+let data9;
+data9 = 5 === '5'; // strict equality operator
+console.log(data9);
+console.log(typeof data9);
+
+let data10;
+data10 = 5 != '5'; // loose inequality operator
+console.log(data10);
+console.log(typeof data10);
+
+// 4. Logical Operators
+
+let data11;
+data11 = (5 > 2) && (3 < 1); // logical AND operator
+console.log(data11);
+console.log(typeof data11);
+
+let data12;
+data12 = (5 > 2) || (3 < 1); // logical OR operator
+console.log(data12);
+console.log(typeof data12);
+
+let data13;
+data13 = !(5 > 2); // logical NOT operator
+console.log(data13);
+console.log(typeof data13);
+
+
+
+
+
+
+
+
