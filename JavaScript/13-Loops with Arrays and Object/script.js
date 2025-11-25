7154ec5af734685c455e2b8a957f86a334bf9455
Mustafa Şeker <mustafa652seker@gmail.com>
2025-11-25 20:10:55 +0300

    feat: enhance loops examples with detailed object handling and mapping

--- JavaScript/13-Loops with Arrays and Object/script.js
+++ JavaScript/13-Loops with Arrays and Object/script.js
@@ -52,3 +52,75 @@ for (let index in cities) {
 }
 
 console.log("----------");
+
+//forEach
+
+cities.forEach(function(city, index) {
+    console.log(index, city);
+});
+
+console.log("----------");
+
+//Objects
+
+let users = [
+    {firstName: "John", lastName: "Doe"},
+    {firstName: "Jane", lastName: "Smith"},
+    {firstName: "Alice", lastName: "Johnson"},
+    {firstName: "Bob", lastName: "Brown"},
+];
+
+for (let i = 0; i < users.length; i++) {
+    console.log(i, users[i].firstName, users[i].lastName);
+}
+
+console.log("----------");
+
+for (let user of users) {
+    console.log(user.firstName, user.lastName);
+}
+
+console.log("----------");
+
+for (let index in users) {
+    console.log(index, users[index].firstName, users[index].lastName);
+}
+
+console.log("----------");
+
+users.forEach(function(user, index) {
+    console.log(index, user.firstName, user.lastName);
+});
+
+console.log("----------");
+
+// for-in with object properties
+
+for (let index in users) {
+    console.log(index);
+    let user = users[index];
+    for (let key in user) {
+        console.log(" ", key, ":", user[key]);
+    }
+    console.log("----------");
+}
+
+
+//map
+
+let fullNames = users.map(function(user) {
+    return user.firstName + " " + user.lastName;
+});
+
+console.log(fullNames);
+
+console.log("----------");
+
+let numberArray = [1, 2, 3, 4, 5];
+
+let squaredNumbers = numberArray.map(function(number) {
+    return number * number;
+});
+
+console.log(squaredNumbers);
+console.log("----------");
