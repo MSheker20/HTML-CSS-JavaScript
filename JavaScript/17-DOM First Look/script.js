d6dcec673ecc04632aa29a3e453b481fc18db496
Mustafa Şeker <mustafa652seker@gmail.com>
2025-12-02 22:54:23 +0300

    feat: add HTML, CSS, and JavaScript files for DOM

---
+++ JavaScript/17-DOM First Look/script.js
@@ -0,0 +1,21 @@
+let data
+
+data = window.document;
+data = window.document.head;
+data = window.document.body;
+data = window.document.title;
+data = window.document.url;
+data = window.document.forms;
+data = window.document.links;
+data = window.document.images;
+data = window.document.forms[0].id;
+data = window.document.forms[0].method;
+data = window.document.forms[0].action;
+data = window.document.forms[0].elements[0].type;
+data = window.document.forms[0].elements[0].name;
+data = window.document.forms[0].elements[0].value;
+
+
+
+
+console.log(data);
