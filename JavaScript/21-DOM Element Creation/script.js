778a5a7dc259c647a9924cc2661143cb83aa5198
Mustafa Şeker <mustafa652seker@gmail.com>
2025-12-03 00:43:28 +0300

    feat: add HTML, CSS, and JavaScript files for Todo List with element creation functionality

---
+++ JavaScript/21-DOM Element Creation/script.js
@@ -0,0 +1,27 @@
+//ELEMENT CREATION
+
+const li = document.createElement("li");
+
+//add class
+li.className = "list-group-item list-group-item-secondary";
+
+//attribute
+li.setAttribute("title", "New Item");
+li.setAttribute("data-id", "5");
+
+const text = document.createTextNode("New List Item");
+li.appendChild(text);
+
+const a = document.createElement("a");
+a.setAttribute("href", "#");
+a.className = "delete-item float-right";
+a.innerHTML = '<i class="fas fa-times"></i>';
+li.appendChild(a);
+
+document.querySelector("#task-list").appendChild(li);
+
+
+
+console.log(li); // <li></li>
+
+
